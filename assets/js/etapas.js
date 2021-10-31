0420cc500c09f5d327126f3a48a4dd1da712080d
patrickhpgc <patriquecordeiro@hotmail.com>
2021-10-31 20:22:58 -0300

    first commit

---
+++ assets/js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulana de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'images/12_2.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrana da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url: 'images/14.jpg',legenda: 'Vereador',small: false}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: 'images/22.jpg', legenda: 'Prefeito', small: false},
+                    {url: 'images/57.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url: 'images/66.jpg', legenda: 'Prefeito', small: false},
+                    {url: 'images/73.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
